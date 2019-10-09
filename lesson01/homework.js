daab9c7a65205a1ba0b205376e17e9b31c758a17
Lyubov <aira_dark@live.ru>
2019-10-09 23:49:53 +0300

    1

--- lesson01/homework.js
+++ lesson01/homework.js
@@ -1,7 +1,7 @@
 const employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
 
-const employersNames = employers.filter(name => name.trim().length > 2)
-              									.map(name => name.toLowerCase().trim());
+const employersNames = employers.filter(name => name.trim().length > 0)
+              					.map(name => name.toLowerCase().trim());
 
 const sponsors = {
     cash: [40000, 5000, 30400, 12000],
@@ -9,15 +9,18 @@ const sponsors = {
     rus: ['RusAuto', 'SBO']
 };
 const {cash, eu, rus} = sponsors;
-const calcCash = (...own) => own.reduce((a, b) => a + b);
+const calcCash = () => cash.reduce((a, b) => a + b);
 
-const money = calcCash(...cash);
-
-const makeBusiness = (owner, cash, emp, director = 'Victor') => {
+const makeBusiness = ({
+    owner = 'Sam',
+    cash = 0,
+    emp = 'nobody',
+    director = 'Victor'
+}) => {
     let sumSponsors = eu.concat(rus, 'unexpected sponsor');
-		console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}
+	console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}
 And we have a sponsors:
 ${sumSponsors}
 Note. Be careful with ${eu[0]}. It's a huge risk.`);
 }
-makeBusiness('Sam', money, employersNames);
+makeBusiness({cash: calcCash(), emp: employersNames});
